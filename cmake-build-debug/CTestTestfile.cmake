a484d26d0556af0fb7c1726b718c339b12c1ae15
MadsNielsen1337 <113089767+MadsNielsen1337@users.noreply.github.com>
2022-11-28 11:21:32 +0100

    Revert "Fisk added"

--- cmake-build-debug/CTestTestfile.cmake
+++ cmake-build-debug/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
-# Source directory: C:/Users/BoBByN/Documents/GitHub/P1
-# Build directory: C:/Users/BoBByN/Documents/GitHub/P1/cmake-build-debug
+# Source directory: C:/Users/Frederik/CLionProjects/P1
+# Build directory: C:/Users/Frederik/CLionProjects/P1/cmake-build-debug
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
