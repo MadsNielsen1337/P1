4a8a9dc7bfde0ddefdfb943b1776f2086e7d317d
emils <esla22@student.aau.dk>
2022-12-01 15:37:06 +0100

    infrastruktur til at lave en linked list med stationsnavne. mangler stadig at kunne sætte de aktuelle navne fra rutedata.txt ind. Stations.h hedder nu routes.h og skal håndtere alt der har med ruter at gøre.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "stations.h"
+#include "routes.h"
 #include "trains.h"
 #include "graph.h"
 
@@ -9,17 +9,7 @@
 #define METER_PER_SECOND_CONVERSION 3.6
 
 #define DATA_SIZE 40
-#define ROUTE_COUNT 3
 #define TRAIN_COUNT 3
-typedef struct route{
-    int distance;
-    int track_speed;
-    char power[DATA_SIZE];
-    char gauge[DATA_SIZE];
-    char controls[DATA_SIZE];
-    char station_start[DATA_SIZE];
-    char station_end[DATA_SIZE];
-} route;
 
 typedef struct train{
     char name[DATA_SIZE];
@@ -53,6 +43,7 @@ void scan_trains(FILE*, train*);
 
 int main(void)
 {
+    /*
     double travel_time = time(IC4.accel,
                               IC4.max_speed,
                               station_distance(Berlin_Hbf, Paris_Nord),
@@ -95,7 +86,7 @@ int main(void)
     // print adjacency list representation of a graph
     printGraph(graph);
 
-
+    */
 
     route routes[ROUTE_COUNT];
     FILE* routefile = fopen("..\\..\\src\\rutedata.txt", "r");
@@ -112,32 +103,6 @@ int main(void)
     fclose(trainfile);
 
     return EXIT_SUCCESS;
-}
-
-
-void scan_routes(FILE* p_file, route* r){   //reads from a file where each line has the following format: Distance,Track_speed,Power,Gauge,Controls,Station_Start,Station_End
-    char temp[DATA_SIZE];
-    for (int i = 0; i < ROUTE_COUNT; ++i) {
-
-        fscanf(p_file, "%[^,]", temp);
-        r[i].distance = strtol(temp, NULL, 10);  //using strtol here to convert the string we've read into a long integer
-        fscanf(p_file, "%*[,]");
-        fscanf(p_file, "%[^,]", temp);
-        r[i].track_speed = strtol(temp, NULL, 10);
-        fscanf(p_file, "%*[,]");
-        fscanf(p_file, "%[^,]", r[i].power);
-        fscanf(p_file, "%*[,]");
-        fscanf(p_file, "%[^,]", r[i].gauge);        //read until a comma is found
-        fscanf(p_file, "%*[,]");                    //dump the comma
-        fscanf(p_file, "%[^,]", r[i].controls);     //read until the next comma
-        fscanf(p_file, "%*[,]");                    //and so on
-        fscanf(p_file, "%[^,]", r[i].station_start);
-        fscanf(p_file, "%*[,]");
-        fscanf(p_file, "%[^\n]", r[i].station_end); //station_end is the last piece of data. a newline character comes right after it
-        fscanf(p_file, "%*[^a-zA-Z0-9]");           //skip characters until a letter or number is reached, which would be at the next line
-    }
-    //printf to test if the format is right. delete later
-    printf("\n%d %d %s %s %s %s %s\n",r[0].distance,r[0].track_speed,r[0].power,r[0].gauge,r[0].controls,r[0].station_start,r[0].station_end);
 }
 
 void scan_trains(FILE* p_file, train* t){   //reads from a file where each line has the following format: Name,Gauge,Control1.Control2.Control3,Fuel1.Fuel2.Fuel3,Acceleration,Max_speed,Passenger_space
@@ -241,7 +206,7 @@ int weight_calc(int avg_track_speed, int distance, char power_f[], char track_ga
 */
     weight += time(IC4.accel,
                    IC4.max_speed,
-                   station_distance(Berlin_Hbf, Paris_Nord),
+                   5,
                    9999,
                    IC4.track_gauge,
                    IC4.control,
