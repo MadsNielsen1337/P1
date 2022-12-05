ee953d023e4a571e183fb1ed06dd47a1e853a06d
Frederik <frederik@pretzmann.dk>
2022-12-05 12:52:52 +0100

    Cleaned UI code up

--- src/UI.c
+++ src/UI.c
@@ -3,7 +3,26 @@
 #include "routes.h"
 
 #define UI_SIZE 61
-#define UI_SPACING 2
+#define SPACING 2
+
+// -----------------------------------------------------------------------------------
+// Local prototypes. Globals are declared in UI.h
+
+void add_spacing(int spacing);
+void print_long_line(int ui_size);
+
+// Garbage collector. Keeps reading the input until a new line (or the end of the file) is reached
+void clear_input(void);
+
+// Draws a UI in the terminal
+void drawMenu(route* routes, station_list_node* list_of_stations);
+
+// Processes the user input
+int menu_choice(route* routes, station_list_node* list_of_stations);
+
+void list_all_stations(station_list_node* list_of_stations);
+
+// -----------------------------------------------------------------------------------
 
 // Garbage collector.
 void clear_input(void){
@@ -12,28 +31,55 @@ void clear_input(void){
     {}
 }
 
-// Draws a UI in the terminal
-void drawMenu(route* routes, station_list_node* list_of_stations) // This function is a nightmare. Needs some cleanup
+void add_spacing(int spacing)
 {
     char drawMenu = '-';
-
-    printf("\n");
-    for (int i = 0; i < UI_SIZE; ++i) {
-        printf("%c", drawMenu);     // Print the UI header
-    }
-    for (int i = 0; i < UI_SPACING - 1; ++i) {
+    for (int i = 0; i < spacing; ++i) {
         printf("\n%c%60c", drawMenu, drawMenu);     // Add space between text
     }
+}
+
+void print_long_line(int ui_size)
+{
+    char drawMenu = '-';
+    printf("\n");
+    for (int i = 0; i < ui_size; ++i) {
+        printf("%c", drawMenu);     // Print the UI header
+    }
+}
+
+// Draws a UI in the terminal
+void drawMenu(route* routes, station_list_node* list_of_stations)
+{
+    char drawMenu = '-';
+    char drawWhitespace = ' ';
+
+    print_long_line(UI_SIZE);
+
+    add_spacing(SPACING - 1);
+
     printf("\n%c UI of European railway simulation%26c", drawMenu, drawMenu);
     printf("\n%c Made by Emil, Eva, Frederik, Louise, Mads & Mohamad%8c", drawMenu, drawMenu);
-    for (int i = 0; i < UI_SPACING; ++i) {
-        printf("\n%c%60c", drawMenu, drawMenu);     // More space
-    }
 
-    for (int i = 0; i < (list_length(list_of_stations)); ++i) {   // Dynamically prints ALL available stations and their index.
-        //printf("\n%c [%d] %s",drawMenu, i, routes[i].station_start);
-        printf("\nS[%d] '%s'", i,index_station_list(list_of_stations, i));
-    }
+    add_spacing(SPACING - 1);
+
+    print_long_line(UI_SIZE);
+
+    add_spacing(SPACING);
+
+    printf("\n%c [a] Run simulation (placeholder)%27c", drawMenu, drawMenu);
+    printf("\n%c [b] See all available stations%29c", drawMenu, drawMenu);
+    printf("\n%c [q] Exit%51c", drawMenu, drawMenu);
+
+    add_spacing(SPACING);
+
+    print_long_line(UI_SIZE);
+
+    printf("\n");
+
+
+
+/* // Tests to see if the format is right
     printf("\n");
     for (int i = 0; i < list_length(list_of_stations); ++i) {
         printf("\nP[%d] '%s'", i, routes[i].power);
@@ -49,23 +95,8 @@ void drawMenu(route* routes, station_list_node* list_of_stations) // This functi
 
     printf("\nlist_length: %d\n", list_length(list_of_stations));
     print_station_list(list_of_stations);
+*/
 
-    printf("\n");
-    for (int i = 0; i < UI_SIZE; ++i) {
-        printf("%c", drawMenu);     // Print long line separation
-    }
-    for (int i = 0; i < UI_SPACING; ++i) {
-        printf("\n%c%60c", drawMenu, drawMenu);     // More space
-    }
-    printf("\n%c [q] Exit%51c", drawMenu, drawMenu);
-    for (int i = 0; i < UI_SPACING; ++i) {
-        printf("\n%c%60c", drawMenu, drawMenu);     // Even more space
-    }
-    printf("\n");
-    for (int i = 0; i < UI_SIZE; ++i) {
-        printf("%c", drawMenu);     // Print UI footer
-    }
-    printf("\n");
 }
 
 // Processes the user input
@@ -85,7 +116,7 @@ int menu_choice(route* routes, station_list_node* list_of_stations) {
      */
     char choice = ' ';
     while (choice != 'q' || choice != 'a' || choice != 'b') {
-        printf("\nChoose menu option: ");
+        printf("\nChoose menu option:");
         scanf("%c", &choice);       // Read input from user
         clear_input();
 
@@ -94,11 +125,27 @@ int menu_choice(route* routes, station_list_node* list_of_stations) {
         else if (choice == 'a') {
             // call a function
             printf("a = fisk\n");
-        } else if (choice == 'b') {
-            // call a function
-            printf("b = fisk2\n");
+        }
+        else if (choice == 'b') {
+            list_all_stations(list_of_stations);
         }
     }
+}
+
+void list_all_stations(station_list_node* list_of_stations)
+{
+    print_long_line(UI_SIZE);
+    printf("\n");
+    printf("\nHere are all available stations:\n");
+
+    // Dynamically prints ALL available stations and their index.
+    for (int i = 0; i < (list_length(list_of_stations)); ++i) {
+        //printf("\n%c [%d] %s",drawMenu, i, routes[i].station_start);
+        printf("\n [%d] %s", i,index_station_list(list_of_stations, i));
+    }
+
+    printf("\n");
+    print_long_line(UI_SIZE);
 }
 
 // Encapsulates all UI functions into one for ease of use
